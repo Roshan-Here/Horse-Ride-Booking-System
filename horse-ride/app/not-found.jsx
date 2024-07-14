210067ccc07262655513329c0c203097ae576878
Muh Roshan <leoparmigiana@gmail.com>
2024-07-14 16:35:01 +0530

    deployment

--- horse-ride/app/not-found.jsx
+++ horse-ride/app/not-found.jsx
@@ -1,6 +1,6 @@
 'use client'
+import Image from 'next/image'
 import React from 'react'
-import { Link } from 'next/link';
 
 function NotFound() {
   return (
@@ -9,12 +9,19 @@ function NotFound() {
                 <div className="w-full lg:w-1/2 mx-8">
                     <div className="m-8 text-7xl text-green-500 font-dark font-extrabold mb-8"> 404</div>
                 <p className="text-2xl md:text-3xl font-light leading-normal mb-8">
-                    Sorry we couldn&apost find the page you're looking for
+                    Sorry we couldn&apost find the page you&aposre looking for
                 </p>
                   <a href="/" className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-red-600 hover:bg-red-700">back to homepage</a>
         </div>
             <div className="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
-            <img src="https://user-images.githubusercontent.com/43953425/166269493-acd08ccb-4df3-4474-95c7-ad1034d3c070.svg" className="" alt="Page not found"></img>
+            <Image 
+            height={500}
+            width={500}
+            src="https://user-images.githubusercontent.com/43953425/166269493-acd08ccb-4df3-4474-95c7-ad1034d3c070.svg" 
+            className=""
+            alt="Page not found">
+
+            </Image>
             </div>
         
         </div>
